71b6d55621cb4f6973779ddba8d126be69f15147
hc5474 <hc5474@g.rit.edu>
2025-04-27 12:55:33 -0400

    Data sampling/splitting completed

--- data_processing/datasampling.py
+++ data_processing/datasampling.py
@@ -1,16 +1,21 @@
 import pandas as pd
 import random
 import os 
+from sklearn.model_selection import train_test_split
 
-random.seed(520)
-full_label_friday_path = "../CICFlowMeter_Processed_Friday_flow_labeled/Friday_two_days_Full_Labeled.csv"
-cleaned_friday_root = "../CICFlowMeter_Processed_Friday_flow_labeled_cleaned"
-cleaned_friday_path = os.path.join(cleaned_friday_root, "two_fridays_labeled_cleaned")
+SEED = 520
+random.seed(SEED)
+
+full_label_friday_path = "./CICFlowMeter_Processed_Friday_flow_labeled/Friday_two_days_Full_Labeled.csv"
+cleaned_friday_root = "./processed_data"
 os.makedirs(cleaned_friday_root, exist_ok=True)
 
+print("Loading Data......")
 df = pd.read_csv(full_label_friday_path)
 print("Data Loaded into dataframe")
 remove_features = ['Flow ID', 'Src IP', 'Src Port', 'Dst IP', 'Dst Port', 'Timestamp', 'Pkt Len Var']
+
+print(f"Dropping Features: {remove_features}")
 
 df = df.drop(columns=remove_features)
 print("Non-useful features dropped")
@@ -23,5 +28,51 @@ print(f"There are {benign_count} Benign flow")
 print(f"There are {attack_count} Malicious flow")
 print("===================")
 
-print("Undersampling Benign flows to be ratio 2:1 to Malicious Flow")
+print("Undersampling Benign flows to be ratio 2:1 to Malicious Flow......")
+
+benign_sample_size = attack_count * 2
+df_benign = df[df['Label'] == 'Benign']
+df_attack = df[df['Label'] != 'Benign']
+
+benign_sampled = df_benign.sample(n=benign_sample_size, random_state=SEED)
+
+print("===================")
+print(f"Sampled Benign flows")
+print(f"Size of Benign Samples: {len(benign_sampled)}")
+print(f"Size of Attack Data: {len(df_attack)}")
+print("===================")
+
+# Spliting data: 70% benign → Training; 15% benign → Validation; 15% benign → Test benign + 100% Test attack
+benign_train, benign_temp = train_test_split(benign_sampled, test_size=0.3, random_state=SEED, shuffle=True)
+benign_train = pd.DataFrame(benign_train)
+benign_temp = pd.DataFrame(benign_temp)
+benign_val, benign_test = train_test_split(benign_temp, test_size=0.5, random_state=SEED, shuffle=True)
+benign_val = pd.DataFrame(benign_val)
+benign_test = pd.DataFrame(benign_test)
+
+test_set = pd.concat([benign_test, df_attack], ignore_index=True)
+
+print("Processing splitted Data into usable form for sklearn......")
+X_train = benign_train.drop(columns=['Label'])
+X_val = benign_val.drop(columns=['Label'])
+X_test = test_set.drop(columns=['Label'])
+y_test = test_set['Label']
+print("======Done======\n")
+
+print("Saving processed data into CSV files...")
+X_train_path = f"{cleaned_friday_root}/X_train.csv"
+X_val_path = f"{cleaned_friday_root}/X_val.csv"
+X_test_path = f"{cleaned_friday_root}/X_test.csv"
+y_test_path = f"{cleaned_friday_root}/y_test.csv"
+
+X_train.to_csv(X_train_path, index=False)
+X_val.to_csv(X_val_path, index=False)
+X_test.to_csv(X_test_path, index=False)
+y_test.to_csv(y_test_path, index=False)
+print("===================")
+print(f"X_train saved to: {X_train_path}")
+print(f"X_val saved to:   {X_val_path}")
+print(f"X_test saved to:  {X_test_path}")
+print(f"y_test saved to:  {y_test_path}")
+print("===================")
 
